f5b5eb09f5186b86a94501f05afcfdcbf2b38003
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-10-04 18:08:46 +0200

    fix: edit

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -3,36 +3,36 @@
 @section('content')
 
 <div class="container">
-    <form action="{{ route('comic.update', compact('comics')) }}" method="POST">
+    <form action="{{ route('comics.update', ['comic' => $comics->id]) }}" method="POST">
         @csrf
         @method('PATCH')
         <div class="form-group">
           <label for="title">Titolo</label>
-          <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}"/>
+          <input type="text" class="form-control" id="title" name="title" value="{{ $comics->title }}"/>
         </div>
         <div class="form-group">
           <label for="description">Descrizione</label>
-          <textarea class="form-control" name="description" id="description" cols="30" rows="10">{{ $comic->description }}"</textarea>
+          <textarea class="form-control" name="description" id="description" cols="30" rows="10">{{ $comics->description }}"</textarea>
         </div>
         <div class="form-group">
           <label for="thumb">thumb</label>
-          <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}"/>
+          <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comics->thumb }}"/>
         </div>
         <div class="form-group">
           <label for="price">Prezzo</label>
-          <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}"/>
+          <input type="text" class="form-control" id="price" name="price" value="{{ $comics->price }}"/>
         </div>
         <div class="form-group">
           <label for="series">Serie</label>
-          <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}"/>
+          <input type="text" class="form-control" id="series" name="series" value="{{ $comics->series }}"/>
         </div>
         <div class="form-group">
           <label for="sale_date">Data Rilascio</label>
-          <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}"/>
+          <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comics->sale_date }}"/>
         </div>
         <div class="form-group">
           <label for="type">Tipologia</label>
-          <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}"/>
+          <input type="text" class="form-control" id="type" name="type" value="{{ $comics->type }}"/>
         </div>
         <button type="submit" class="btn btn-success">Aggiungi Nuovo Fumetto</button>
       </form>
